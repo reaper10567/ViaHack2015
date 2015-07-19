ec4545b1b84148245d896083466d4f3091050e15
mattwilmes <matthew.wilmes@viasat.com>
2015-07-19 06:04:23 -0400

    updated

--- Suwarna_test/index.php
+++ Suwarna_test/index.php
@@ -14,7 +14,7 @@ $id = $_GET['id'];
 
 
 if($id == "1"){
-$eventName = "ViaSat Hackathon";
+$eventName = $_GET['eventName'];
 $db = new SQLite3('Events.sqlite');
 $lists = [];
 
@@ -26,15 +26,5 @@ while ($row = $results1->fetchArray()){
 
 echo json_encode($lists);
 }
-
-
-// Here's some data that we want to send via JSON.
-// We'll include the $id parameter so that we
-// can show that it has been passed in correctly.
-// You can send whatever data you like.
-$data = array("Hello", $id);
-
-// Send the data.
-echo json_encode($data);
 
 ?>
